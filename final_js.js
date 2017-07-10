a37919c8e82384544ccf9c9a1e2e6d99aa895b90
Flint64 <twda96@gmail.com>
2017-07-10 11:02:00 -0600

    Saving

--- final_js.js
+++ final_js.js
@@ -1,7 +1,19 @@
-home_button.onclick = function(){
-	document.getElementById("text").innerHTML = "Something else";
-};
-
-registration.onclick = function(){
-	alert("something");
+function getJSON(url) {
+    return fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+		
+		ajaxFromLocalJson();
+	
+	
+function ajaxFromLocalJson(){
+	getJSON("final_json.json").then(function (data){
+		console.log(data);
+				
+	});
 }
