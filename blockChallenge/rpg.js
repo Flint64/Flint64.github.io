7086f0a30b70b23743c1af048c49ef48e0b832fb
Flint64 <twda96@gmail.com>
2019-01-31 10:05:15 -0700

    fixed rpg CSS

--- blockChallenge/rpg.js
+++ blockChallenge/rpg.js
@@ -31,7 +31,7 @@ settingsButton.addEventListener("touchend", e => {
 });
 
 
-  var requestURL = 'http://127.0.0.1:5500/Block%20Challenge/test.json';
+  var requestURL = 'http://127.0.0.1:5500/test.json';
   var request = new XMLHttpRequest();
   request.open('GET', requestURL);
   request.responseType = 'json';
