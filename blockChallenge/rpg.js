c5d11c3cd3d7591c3bdd1379020592f503497c72
Flint64 <twda96@gmail.com>
2019-01-31 17:02:19 -0700

    s

--- blockChallenge/rpg.js
+++ blockChallenge/rpg.js
@@ -2,11 +2,13 @@ let menuButton = document.getElementById("menuButton");
 let userArea = document.getElementById("userArea");
 let statusBar = document.getElementById("statusBar");
 let storyArea = document.getElementById("storyArea");
-let choiceButtons = document.getElementById("choiceButtons")
-.querySelectorAll("button");
+let choiceButtons = document.getElementById("choiceButtons");
 let menu = document.getElementById("menu");
 let settingsButton = document.getElementById("settings");
+let choiceLeft = document.getElementById("choiceLeft");
+let choices = document.getElementById("menuChoices");
 
+/*
 menuButton.addEventListener("touchend", e => {
   let i = 0,
     length = choiceButtons.length;
@@ -25,6 +27,21 @@ menuButton.addEventListener("touchend", e => {
     }
   }
 });
+*/
+
+menuButton.addEventListener("touchend", e => {
+
+  if (choiceButtons.style.opacity == 1){
+    menu.innerHTML = "Close";
+    statusBar.style.height = "10.7em";
+    choiceButtons.style.opacity = 0;
+  } else {
+    menu.innerHTML = "Menu";
+    statusBar.style.height = "6em";
+    choiceButtons.style.opacity = 1;
+  }
+
+});
 
 settingsButton.addEventListener("touchend", e => {
   statusBar.style.height = "90%";
@@ -37,9 +54,49 @@ settingsButton.addEventListener("touchend", e => {
   request.responseType = 'json';
   request.send();
 
-  var superHeroes;
+  /*
+*   Creates the story variable to hold the JSON object,
+*   then loops through the first instance of the object
+*   and grabs the text and creates a <p> for each one and
+*   puts the text into the page then puts the <p> into the page
+  */
+
+//Story[0] is just the first array.
+ function start() {
+  for (k in story[0].text) {
+    var p = document.createElement("p");
+    var content = document.createTextNode(story[0].text[k]);
+    p.appendChild(content);
+    storyArea.appendChild(p);
+    }
+
+    if (story[0].continue == true){
+      var button = document.createElement("Button");
+      var text = document.createTextNode("Continue");
+      button.appendChild(text);
+      choiceButtons.appendChild(button);
+    } else {
+      for (var i = 0; i < story[0].choice.length / 2; i++){
+        var button = document.createElement("Button");
+        var text = document.createTextNode(story[1].choice[i]);
+        button.appendChild(text);
+        choiceLeft.appendChild(button);
+      }
+      for (var i = 0; i < story[0].choice.length / 2; i++){
+        var button = document.createElement("Button");
+        var text = document.createTextNode(story[1].choice[i]);
+        button.appendChild(text);
+        choiceRight.appendChild(button);
+      }
+    }
+  }
+
+  var story;
 
   request.onload = function() {
-    superHeroes = request.response;
-    console.log(superHeroes);
+    story = request.response;
+    console.log(story);
+
+    start();
+
   }
